0aba3b204a4c59378fd4b537ab0f563d3dab7e58
Brigtht <brigth_kt@Brigthts-MacBook-Pro.local>
2023-06-03 23:38:57 +0700

    database

--- main.go
+++ main.go
@@ -7,42 +7,49 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
+
+	"github.com/gin-gonic/gin"
 )
 
 var (
-	server *gin.Engine
-	us		service.RelatedNumberService
-	uc		controller.RelatedNumberController
-	ctx		context.Context
-	userc	*mongo.Collection
-	mongoclient *mongo.Client
-	err		error
+ 	server      *gin.Engine
+ 	us          service.RelatedNumberService
+ 	uc          controller.RelatedNumberController
+ 	ctx         context.Context
+	userc       *mongo.Collection
+ 	mongoclient *mongo.Client
+ 	err         error
 )
 
 func init() {
-	ctx = context.TODO()
+	clientOptions := options.Client().ApplyURI("mongodb://username:password@202.151.177.207:27017").SetAuth(options.Credential{
+		AuthSource: "battery",
+		Username:   "dev",
+		Password:   "12345678",
+	})
 
-	mongoconn := options.Client().ApplyURI("mongodb://localhost:27017")
-	mongoclient, err = mongo.Connect(ctx, mongoconn)
+	// Connect to MongoDB server.
+	mongoclient, err = mongo.Connect(context.Background(), clientOptions)
 	if err != nil {
-		log.Fatal("error while connecting with mongo", err)
+		log.Fatal(err)
 	}
-	err = mongoclient.Ping(ctx, readpref.Primary())
+
+	// Check the connection.
+	err = mongoclient.Ping(context.Background(), nil)
 	if err != nil {
-		log.Fatal("error while trying to ping mongo", err)
+		log.Fatal(err)
 	}
 
-	fmt.Println("mongo connection established")
+	fmt.Println("Connected to MongoDB server.")
 
-	userc = mongoclient.Database("api").Collection("Battery-Tracking")
+	userc = mongoclient.Database("battery").Collection("Battert-Tracking")
 	us = service.NewRelatedNumberService(userc, ctx)
 	uc = controller.New(us)
 	server = gin.Default()
 }
+
 
 func main() {
 	defer mongoclient.Disconnect(ctx)
@@ -52,3 +59,4 @@ func main() {
 
 	log.Fatal(server.Run(":9099"))
 }
+
